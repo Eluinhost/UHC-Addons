4db4036679bf8f55f223b4322152a42df5a09d9c
HowdenG <graham_howden1@yahoo.co.uk>
2014-09-03 11:16:26 +0100

    new register format

--- scatter/src/main/java/com/publicuhc/uhcaddons/scatter/UhcScatterPlugin.java
+++ scatter/src/main/java/com/publicuhc/uhcaddons/scatter/UhcScatterPlugin.java
@@ -1,16 +1,32 @@
 package com.publicuhc.uhcaddons.scatter;
 
 import com.publicuhc.ultrahardcore.UltraHardcore;
+import com.publicuhc.ultrahardcore.api.Command;
+import com.publicuhc.ultrahardcore.api.UHCAddonConfiguration;
+import com.publicuhc.ultrahardcore.api.UHCFeature;
+import com.publicuhc.ultrahardcore.framework.shaded.inject.multibindings.Multibinder;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UhcScatterPlugin extends JavaPlugin {
+public class UhcScatterPlugin extends JavaPlugin implements UHCAddonConfiguration
+{
 
     @Override
     public void onEnable()
     {
         UltraHardcore uhc = (UltraHardcore) Bukkit.getPluginManager().getPlugin("UltraHardcore");
+        uhc.registerAddon(this, this);
+    }
 
-        uhc.registerAddon(new ScatterModule(this));
+    @Override
+    public void configureCommands(Multibinder<Command> commandMultibinder)
+    {
+        commandMultibinder.addBinding().to(ScatterCommand.class);
+    }
+
+    @Override
+    public void configureFeatures(Multibinder<UHCFeature> uhcFeatureMultibinder)
+    {
+        //no features enabled
     }
 }
